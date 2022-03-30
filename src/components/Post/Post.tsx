e8fbeb14f7ec2d132dd2c3c21048e3141cfdcde4
Leonel Meque <leomeque@gmail.com>
2022-03-30 09:12:10 +0200

    feat: add post image component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -29,25 +29,23 @@ const FeedPost = ({
     postId,
     downloadURL,
     ...rest
-}: FeedPostProps) => {
-    return (
-        <StyledView>
-            <PostHeader username={rest.user.username} />
-            <PostImage
-                source={{
-                    uri: downloadURL,
-                }}
-            />
-            <PostSocialActions hasLike={isLiked} isBookmarked={isSaved} />
-            <PostLikes likes={likes} />
-            <View>
-                <PostCaption username={rest.user.username} caption={rest.caption} />
-                <PostComments comments={comments} />
-                <PostDate>{postDate}</PostDate>
-            </View>
-        </StyledView>
-    );
-};
+}: FeedPostProps) => (
+    <StyledView>
+        <PostHeader username={rest.user.username} />
+        <PostImage
+            source={{
+                uri: downloadURL,
+            }}
+        />
+        <PostSocialActions hasLike={isLiked} isBookmarked={isSaved} />
+        <PostLikes likes={likes} />
+        <View>
+            <PostCaption username={rest.user.username} caption={rest.caption} />
+            <PostComments comments={comments} />
+            <PostDate>{postDate}</PostDate>
+        </View>
+    </StyledView>
+);
 
 const PostDate = styled.Text`
     font-size: 12px;
@@ -55,31 +53,15 @@ const PostDate = styled.Text`
     margin: 4px 0px;
 `;
 
-const TotalComments = styled.Text`
-    color: #707070;
-    margin: 6px 0px;
-`;
-
 const PostImage = styled.Image`
     width: 100%;
     height: 400px;
 `;
+
 const StyledView = styled.View`
     width: 100%;
     /* height: 400px; */
     /* background-color:blue; */
 `;
-
-const UserProfileName = styled.Text`
-    font-weight: 600;
-    font-size: 14px;
-`;
-
-const TopComments = styled.View`
-    flex-direction: row;
-    padding: 4px 0px;
-`;
-
-const UserCommentInput = styled.TextInput``;
 
 export default FeedPost;
---
+++ src/components/Post/PostImage.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent } from "react";
+import styled from "styled-components/native";
+
+interface PostImageProps {
+    [key: string]: any;
+}
+
+
+// Stateful component
+const PostImage: FunctionComponent<PostImageProps> = (props) => {
+    return (
+        <StyledPostImage
+            source={{
+                uri: props.downloadURL,
+            }}
+        />
+    );
+};
+
+const StyledPostImage = styled.Image`
+    width: 100%;
+    height: 400px;
+`;
+
+export default PostImage;
