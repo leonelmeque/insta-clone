6063aaf016b3dff1a3d6abab3472237c24907854
Leonel Meque <leomeque@gmail.com>
2022-07-18 22:59:02 +0200

    feat(hooks): add a function to check login status

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import firebase from 'firebase';
+import { useUser } from 'context/user-context';
+import { useNavigation } from '@react-navigation/native';
 
-export function useAuth(){
-   
+export function useAuth() {
+    const [userState, userDispatch] = useUser()
+    const navigation = useNavigation()
+
+    const onCheckLoginStatus = () => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (!user) return
+
+            userDispatch({
+                type: 'USER_STATE_CHANGE',
+                payload: {
+                    user: {
+                        uid: user?.uid
+                    }
+                }
+            })
+            //  @ts-ignore
+            navigation.navigate("global/main")
+
+        });
+
+    }
+
     const onSignIn = (email: string, password: string) => {
         return firebase
             .auth()
             .signInWithEmailAndPassword(email, password)
             .then((result) => {
-                return result
+                console.log("signin success")
+
+                userDispatch({
+                    type: 'USER_STATE_CHANGE',
+                    payload: {
+                        user: {
+                            uid: result.user?.uid
+                        }
+                    }
+                })
+                // @ts-ignore
+                navigation.navigate("global/main")
             })
             .catch((result) => {
-    
+
             });
     };
 
@@ -34,13 +68,18 @@ export function useAuth(){
             });
     };
 
-     const onLogOut = () => {
-        return firebase.auth().signOut().then((result) => { }).catch((err) => { })
+    const onLogOut = async () => {
+        return await firebase.auth().signOut().then((result) => {
+            userDispatch({
+                type: 'REMOVE_USER_FROM_STATE',
+            })
+        }).catch((err) => { })
     }
-    
-  return {
-    onSignIn,
-    onLogOut,
-    onSignUp
-  }
+
+    return {
+        onSignIn,
+        onLogOut,
+        onSignUp,
+        onCheckLoginStatus
+    }
 }
