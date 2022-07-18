e54b8bca1ac9dcb69cfe8f5dff9d07b97897f660
Leonel Meque <leomeque@gmail.com>
2022-07-18 23:00:08 +0200

    refactor(App.tsx): made the code better

--- App.tsx
+++ App.tsx
@@ -1,97 +1,37 @@
 // React imports
 import React from "react";
-import { useEffect } from "react";
-import { Text, View } from "react-native";
 import { useState } from "react";
-import { useFonts } from "expo-font";
 
 // Navigation imports
 import { NavigationContainer } from "@react-navigation/native";
-import { LandingScreenNavigation } from "navigation/landing";
-import { GlobalNavigation } from "navigation/main";
 
 // Service imports
 import firebaseInit from "library/firebaseConfig";
-import * as firebase from "firebase";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 // Redux imports
 import { Provider } from "react-redux";
 import ThemeProvider from "theme/context";
 import { theme } from "theme/theme";
 import store from "store";
-import { UserProvider } from "context/user-context";
-// Creating redux store
+import { UserProvider, useUser } from "context/user-context";
 
+import Container from "./Container";
 
 //Initializing firebase
 firebaseInit();
 
-const initializeLogin =  async () => {
-    await firebase.default.auth().onAuthStateChanged((user) => {
-        if (!user) {
-            return {
-                loggedIn: false,
-                loaded: true
-            }
-        }
-
-    });
-    return {
-        loggedIn: true,
-        loaded: true,
-    };
-}
-
 export default function App() {
     const [darkMode, setDarkMode] = useState<boolean>(false);
-    const [state, setState] = useState<{
-        loggedIn?: boolean;
-        loaded?: boolean;
-    }>({});
-
-    let [fontsLoaded] = useFonts({
-        "MerriweatherSans-Regular": require("assets/fonts/MerriweatherSans-Regular.ttf"),
-    });
-
-    useEffect(() => {
-        if (!state.loggedIn) {
-           initializeLogin().then(result=>{
-                setState(result)
-            })
-           
-        }
-    }, [state]);
-
-    if (!state?.loaded && !fontsLoaded) {
-        return (
-            <SafeAreaView>
-                <View>
-                    <Text>Loading app</Text>
-                </View>
-            </SafeAreaView>
-        );
-    }
-
-    if (!state?.loggedIn) {
-        return (
-            <UserProvider>
-                <ThemeProvider.Provider value={darkMode ? {} : theme}>
-                    <NavigationContainer>
-                        <LandingScreenNavigation />
-                    </NavigationContainer>
-                </ThemeProvider.Provider>
-            </UserProvider>
-        );
-    }
 
     return (
-        <ThemeProvider.Provider value={darkMode ? {} : theme}>
-            <Provider store={store}>
-                <NavigationContainer>
-                    <GlobalNavigation />
-                </NavigationContainer>
-            </Provider>
-        </ThemeProvider.Provider>
+        <UserProvider>
+            <ThemeProvider.Provider value={darkMode ? {} : theme}>
+                <Provider store={store}>
+                    <NavigationContainer>
+                       <Container />
+                    </NavigationContainer>
+                </Provider>
+            </ThemeProvider.Provider>
+        </UserProvider>
     );
 }
--- src/screens/auth/Landing.tsx
+++ src/screens/auth/Landing.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import { SafeAreaView } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { LandingScreenNavigationParams } from "navigation/types";
-import { onSignIn } from "library/backend";
 import LoginForm from "components/organisms/LoginForm";
+import { useAuth } from "hooks";
 
 interface LandingProps
     extends NativeStackScreenProps<LandingScreenNavigationParams, "landing/login"> {}
 
 const Landing: FunctionComponent<LandingProps> = ({ navigation }) => {
+    const {onSignIn} = useAuth()
     return (
         <SafeAreaView>
             <LoginForm
