927fe9105129c0a5d0715b7fe05702a7b1276cac
Leonel Meque <leomeque@gmail.com>
2022-07-19 10:02:31 +0200

    refactor(molecules): profile action molecule with better api

--- src/components/molecules/Profile/ProfileActions.tsx
+++ src/components/molecules/Profile/ProfileActions.tsx
@@ -1,51 +1,17 @@
 import React from "react";
 import { FunctionComponent } from "react";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { useState } from "react";
-import { StackParamsList } from "navigation/types";
-import { useEffect } from "react";
-import {
-    followUser,
-    unFollowUser,
-    isFollowing,
-} from "library/backend";
 import firebase from "firebase";
 import Button from "components/atoms/Button";
 import Box from "components/atoms/Box";
-
-interface ProfileActionsProps
-    extends NativeStackScreenProps<StackParamsList, "tabs/profile"> {}
+import { useUserActions } from "hooks/use-user-actions";
+interface ProfileActionsProps {
+    uid: string
+}
 
 const ProfileActions: FunctionComponent<ProfileActionsProps> = ({
-    route,
-    navigation,
+    uid,
 }) => {
-    const [follows, setFollows] = useState<boolean | null>(null);
-
-    // Fetching user profile
-    const fetchFollowStatus = async () => {
-        const result = await isFollowing(route?.params?.uid);
-        setFollows(result);
-    };
-
-    const userFollowOrUnfollow = () => {
-        if (!follows) {
-            followUser(route?.params?.uid).then(() => {
-                setFollows(!follows);
-            });
-        } else {
-            unFollowUser(route?.params?.uid).then(() => {
-                setFollows(!follows);
-            });
-        }
-    };
-
-    useEffect(() => {
-        if (route?.params?.uid) {
-            if (!follows) fetchFollowStatus();
-            return;
-        }
-    }, []);
+    const {unfollowUser, followUser, follows } = useUserActions(uid)
 
     return (
         <Box
@@ -54,10 +20,11 @@ const ProfileActions: FunctionComponent<ProfileActionsProps> = ({
                 justifyContent: "space-between",
                 alignItems: "stretch",
             }}>
-            {route?.params?.uid && route.params.uid !== firebase.auth().currentUser?.uid && (
+            {uid !== firebase.auth().currentUser?.uid && (
                 <Button
                     onPress={() => {
-                        userFollowOrUnfollow();
+                        if(follows) unfollowUser()
+                        else followUser()
                     }}
                     style={{
                         flex: 1,
