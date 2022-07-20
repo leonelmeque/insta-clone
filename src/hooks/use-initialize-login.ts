c0cff17e33d7b3add110f9a5b07b00cc80070289
Leonel Meque <leomeque@gmail.com>
2022-07-20 09:41:41 +0200

    feat(hooks): add fetchUserInfo to get information from the user

--- src/hooks/use-initialize-login.ts
+++ src/hooks/use-initialize-login.ts
@@ -1,18 +1,50 @@
 import { useAuth } from "./use-auth";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
-import { useFetchUser } from "./use-fetch-user";
+import { useUser } from "context";
+import { fetchUsersPosts, getFirebaseUser } from "library/backend";
+import { useDispatch } from "react-redux";
+import { fetchUserInfo } from "library/backend";
 
 export const useInitializeLogin = () => {
-  const {onCheckLoginStatus} = useAuth()
+  const { onCheckLoginStatus } = useAuth()
+  const [_, userDispatch] = useUser()
+
+  const initUser = async () => {
+    const result = await getFirebaseUser()
+    const userInfo = await fetchUserInfo()
+   
+    userDispatch({
+      type: 'USER_STATE_CHANGE',
+      payload: {
+        user: {
+          ...result
+        },
+        ...userInfo
+
+      }
+    })
+  }
+
+  const initUserPosts = async () => {
+    const result = await fetchUsersPosts()
+    userDispatch({
+      type: 'USER_POSTS_STATE_CHANGE',
+      payload: {
+        posts: result
+      }
+    })
+  }
 
   let [fontsLoaded] = useFonts({
-      "MerriweatherSans-Regular": require("assets/fonts/MerriweatherSans-Regular.ttf"),
+    "MerriweatherSans-Regular": require("assets/fonts/MerriweatherSans-Regular.ttf"),
   });
 
-  useEffect(()=>{
-      onCheckLoginStatus()
-  },[])
+  useEffect(() => {
+    onCheckLoginStatus()
+    initUser()
+    initUserPosts()
+  }, [])
 
   return { fontsLoaded }
 }
