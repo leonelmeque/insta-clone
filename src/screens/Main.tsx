412aad6447e4cd4733c74b76a249856aba4392f2
Leonel Meque <leomeque@gmail.com>
2022-07-20 09:42:16 +0200

    refactor(screens): removed redux from main screen

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -4,39 +4,9 @@ import { FunctionComponent } from "react";
 
 // Navigation imports
 import { AppTabNavigation } from "navigation/tabs";
-import { UserState } from "store/reducers/user-reducer";
-import { bindActionCreators, Dispatch } from "redux";
-import { loadData } from "store/actions";
-import { connect, ConnectedProps } from "react-redux";
 
-type RootState = {
-    userState: UserState;
-};
-
-const mapStateToProps = (store: RootState): UserState => ({
-    user: store.userState.user,
-    posts: store.userState.posts,
-    following: store.userState.following,
-    feed: [],
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) =>
-    bindActionCreators({ loadData }, dispatch);
-
-const connector = connect(mapStateToProps, mapDispatchToProps)
-
-type PropsFromRedux = ConnectedProps<typeof connector>;
-
-interface Props extends PropsFromRedux {}
-
-const MainScreen: FunctionComponent<Props> = (props) => {
-
-    useEffect(() => {
-        props.loadData();
-        return () => {};
-    }, []);
-
+const MainScreen = () => {
     return <AppTabNavigation />;
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainScreen);
+export default MainScreen;
