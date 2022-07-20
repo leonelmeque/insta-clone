7deb74e0e8be0afee5a68c1aad7e9c646d10854a
Leonel Meque <leomeque@gmail.com>
2022-07-20 21:28:05 +0200

    fix(hooks): bad state format

--- src/hooks/use-fetch-user.ts
+++ src/hooks/use-fetch-user.ts
@@ -1,24 +1,26 @@
 import { useUser } from 'context/user-context'
-import { getUser } from 'library/backend'
+import { fetchUserInfo, getUser } from 'library/backend'
 import { useEffect, useState } from 'react'
 
-
-export const useFetchUser = (uid: string) => {
+export const useFetchUser = (uid: string, shouldRefresh: boolean) => {
   const [userState, _] = useUser()
-  const [state, setState] = useState<{uid:string, [key:string]: any} | undefined>()
+  const [state, setState] = useState<{ uid: string, [key: string]: any } | undefined>()
 
   const fetchUser = async () => {
     const id = uid === undefined ? userState?.user?.uid : uid
-    const results = await getUser(id)
+    const user = await getUser(id as string)
+    const userInfo = await fetchUserInfo(id)
+
     setState({
-      uid:id,
-      ...results
+      uid: id as string,
+      ...user,
+      ...userInfo
     })
   }
 
   useEffect(() => {
     fetchUser()
-  }, [])
+  }, [uid, shouldRefresh])
 
   return state
 }
